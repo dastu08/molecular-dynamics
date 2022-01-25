03dfb1645e8f7bb2acd086f3e3682cf3cdf98d05
dastu08 <37212582+dastu08@users.noreply.github.com>
2022-01-25 21:20:22 +0100

    Changed testing parameters

--- main.cpp
+++ main.cpp
@@ -14,19 +14,19 @@ const double sigma = 3.4;
 int main() {
     // report 1. dynamics
 #ifdef REPORT_1_1
-    uint num_samples = 200;
-    Eigen::Array<double, Eigen::Dynamic, 5> data(num_samples, 5);
-    MD::lj_test(data, num_samples);
-    MD::array2file(data, "../reports/lj_test.txt", "x,energy,fx,fy,fz");
+    uint num_samples = 400;
+    Eigen::Array<double, Eigen::Dynamic, 5> data_lj(num_samples, 5);
+    MD::lj_test(data_lj, num_samples);
+    MD::array2file(data_lj, "../reports/lj_test.txt", "x,energy,fx,fy,fz");
 #endif
 
 #ifdef REPORT_1_2
-    double time_step = 0.001;
-    uint num_t_steps = 100;
+    double time_step = 0.01;
+    uint num_t_steps = 800;
     uint num_particles = 2;
     Eigen::ArrayX3d positions{{0, 0, 0}, {4 / sigma, 0, 0}};
     Eigen::ArrayX3d velocities{{0, 0, 0}, {0, 0, 0}};
-    Eigen::ArrayXXd data = Eigen::ArrayXXd::Zero(num_t_steps, 7);
+    Eigen::ArrayXXd data_vv = Eigen::ArrayXXd::Zero(num_t_steps, 7);
 
     MD::velocity_verlet(positions,
                         velocities,
@@ -35,9 +35,9 @@ int main() {
                         num_t_steps,
                         num_particles,
                         MD::sample_x,
-                        data);
+                        data_vv);
 
-    MD::array2file(data, "../reports/vv_test.txt", "t,x1,v1,x2,v2,epot,ekin");
+    MD::array2file(data_vv, "../reports/vv_test.txt", "t,x1,v1,x2,v2,epot,ekin");
 
     // std::cout << positions << std::endl;
 
