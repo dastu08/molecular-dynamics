9fd1f056c6c55d6bb6bf597120f04096475ec424
dastu08 <37212582+dastu08@users.noreply.github.com>
2022-01-25 13:04:41 +0100

    Update description
    Reduced units for forces added.

--- components/potentials.h
+++ components/potentials.h
@@ -17,9 +17,9 @@ namespace MD {
 // Description:
 //  For each particle pair compute the forces from the Lennard-Jones potential
 //  and sum them all up. Also compute the total potential energy of the system.
-//  $V_{LJ}(r) = 4 eps ( (sigma/r)^{12} - (sigma/r)^6 )$
-//  The implementation has positions in units of sigma and the energy/forces
-//  in units of eps.
+//  $V_{LJ}(r) = 4 epsilon ( (sigma/r)^{12} - (sigma/r)^6 )$
+//  The implementation has positions in units of sigma, energy in units of
+//  epsilon and forces in units of epsilon/sigma.
 double lennard_jones(const Eigen::ArrayX3d &positions,
                      Eigen::ArrayX3d &forces,
                      uint num_particles);
