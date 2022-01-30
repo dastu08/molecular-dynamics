bcf7bdc8bd4e5ddf02e6a56e97fae26df965bc3c
dastu08 <37212582+dastu08@users.noreply.github.com>
2022-01-30 16:25:39 +0100

    Cleaned up main for submission

--- main.cpp
+++ main.cpp
@@ -6,37 +6,14 @@
 #include "components/solvers.h"
 #include "components/tests.h"
 
-// #define REPORT_1
-#define MANY_PARTICLES
+#define REPORT_1
 
 // see Rahman for the constant
 const double sigma = 3.4;
 
-void sample_xy5(uint index,
-                double time,
-                Eigen::ArrayX3d &positions,
-                Eigen::ArrayX3d &velocities,
-                double e_pot,
-                Eigen::ArrayXXd &data) {
-    // if ((index % 2) == 1) {
-    //     return;
-    // }
-
-    data(index, 0) = time;
-    data(index, 1) = e_pot;
-    data(index, 2) = velocities.square().sum() / 2;
-    // x position
-    for (int i = 0; i < 5; i++) {
-        data(index, 3 + i) = positions(i, 0);
-        data(index, 8 + i) = positions(i, 1);
-        data(index, 13 + i) = velocities(i, 0);
-        data(index, 18 + i) = velocities(i, 1);
-    }
-}
-
 int main() {
-#ifdef REPORT_1
     // Report 1. Dynamics
+#ifdef REPORT_1
 
     // Potential test
     uint num_samples = 1000;
@@ -62,32 +39,6 @@ int main() {
     MD::time_step_test(data_ts, 0.01, 0.06, time_step_samples);
     MD::array2file(data_ts, "../data/01/ts_test_index.txt", "index,ts");
 #endif  // REPORT_1
-
-#ifdef MANY_PARTICLES
-    // Example for plotting the forces of some particles
-    uint num_particles = 5;
-    uint num_t_steps = 160000;
-    double time_step = 0.0001;
-    Eigen::ArrayX3d positions{{0, 0, 0},
-                              {2, 0, 0},
-                              {4.1, 0, 0},
-                              {6, 0, 0},
-                              {8, 0, 0}};
-    Eigen::ArrayX3d forces;
-    Eigen::ArrayX3d velocities = Eigen::ArrayX3d::Zero(num_particles, 3);
-    Eigen::ArrayXXd data(num_t_steps, 3 + 4 * num_particles);
-
-    // compute the static forces
-    MD::lennard_jones(positions, forces, num_particles);
-    MD::array2file((Eigen::ArrayXXd(num_particles, 6) << positions, forces).finished(),
-                   "../reports/many_particles_forces.txt", "x,y,z,fx,fy,fz");
-
-    // simulate the system
-    MD::velocity_verlet(positions, velocities, MD::lennard_jones, time_step,
-                        num_t_steps, num_particles, sample_xy5, data);
-    MD::array2file(data, "../reports/many_particles_evolution.txt",
-                   "t,epot,ekin,x1,x2,x3,x4,x5,y1,y2,y3,y4,y5,vx1,vx2,vx3,vx4,vx5,vy1,vy2,vy3,vy4,vy5");
-#endif  // MANY_PARTICLES
 
     return 0;
 }
