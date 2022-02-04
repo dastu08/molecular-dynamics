70a6c164c76ddf076e8384e17157aad3c2c92d5d
dastu08 <37212582+dastu08@users.noreply.github.com>
2022-02-04 20:48:41 +0100

    overload off velocity verlet for MIC (minimal image convention)
    Can use the MIC-LJ potential with velocity verlet.

--- components/solvers.cpp
+++ components/solvers.cpp
@@ -49,6 +49,54 @@ void velocity_verlet(Eigen::ArrayX3d &positions,
     }
 }
 
+void velocity_verlet(Eigen::ArrayX3d &positions,
+                     Eigen::ArrayX3d &velocities,
+                     double (*force)(const Eigen::ArrayX3d &,
+                                     Eigen::ArrayX3d &,
+                                     uint,
+                                     double),
+                     double time_step,
+                     uint num_t_steps,
+                     uint num_particles,
+                     void (*sampler)(uint,
+                                     double,
+                                     Eigen::ArrayX3d &,
+                                     Eigen::ArrayX3d &,
+                                     double,
+                                     Eigen::ArrayXXd &),
+                     Eigen::ArrayXXd &data,
+                     double mic_length) {
+    Eigen::ArrayX3d forces;
+    double e_pot;
+    double t = 0;
+
+    if (positions.size() != velocities.size()) {
+        std::cout << "[Error] The sizes of positions and velocties don't match.\
+            Aborting veloctiy verlet algorithm !"
+                  << std::endl;
+        return;
+    }
+
+    // initial energy/force calculation
+    e_pot = force(positions, forces, num_particles, mic_length);
+    sampler(0, t, positions, velocities, e_pot, data);
+
+    // run the integration over time
+    for (uint i = 1; i < num_t_steps; i++) {
+        // assume forces are already for current positons
+        positions += velocities * time_step + forces * time_step * time_step / 2;
+        // add last force term
+        velocities += forces * time_step / 2;
+        e_pot = force(positions, forces, num_particles, mic_length);
+        // add current force term
+        velocities += forces * time_step / 2;
+        t += time_step;
+        // call the sampler to save quantities in data
+        sampler(i, t, positions, velocities, e_pot, data);
+    }
+}
+
+
 void sample_x2(uint index,
                double time,
                Eigen::ArrayX3d &positions,
