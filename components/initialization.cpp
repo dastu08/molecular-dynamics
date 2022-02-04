5fe80b5e9ad9eaa0e573e50604dbf8e9606f2d6c
dastu08 <37212582+dastu08@users.noreply.github.com>
2022-02-04 09:18:34 +0100

    Velocity initialization - random velocities - drift removal

--- components/initialization.cpp
+++ components/initialization.cpp
@@ -1,17 +1,21 @@
 #include "initialization.h"
 
+#include <stdlib.h>
+
+#include <iostream>
+
 namespace MD {
 
-void init3d(Eigen::ArrayX3d &positions,
-            uint num_paricles_cubic,
-            double distance) {
+uint init_potisionts_3d(Eigen::ArrayX3d &positions,
+                        uint num_paricles_cubic,
+                        double distance) {
     uint num_particles = num_paricles_cubic * num_paricles_cubic * num_paricles_cubic;
     uint index;
     positions = Eigen::ArrayX3d::Zero(num_particles, 3);
 
-    for (uint ix = 0; ix < num_paricles_cubic; ix++) {
-        for (uint iy = 0; iy < num_paricles_cubic; iy++) {
-            for (uint iz = 0; iz < num_paricles_cubic; iz++) {
+    for (uint ix = 0; ix < num_paricles_cubic; ++ix) {
+        for (uint iy = 0; iy < num_paricles_cubic; ++iy) {
+            for (uint iz = 0; iz < num_paricles_cubic; ++iz) {
                 index = (ix * num_paricles_cubic + iy) * num_paricles_cubic + iz;
                 positions(index, 0) = ix * distance;
                 positions(index, 1) = iy * distance;
@@ -19,6 +23,28 @@ void init3d(Eigen::ArrayX3d &positions,
             }
         }
     }
+
+    return num_particles;
+}
+
+void init_velocities_3d(Eigen::ArrayX3d &velocities,
+                        uint num_particles,
+                        double max_veloctiy,
+                        uint seed) {
+    // init random number generator
+    srand(seed);
+    for (uint i = 0; i < num_particles; ++i) {
+        for (uint j = 0; j < 3; j++) {
+            velocities(i, j) = ((double)rand() / RAND_MAX - 0.5) * 2 * max_veloctiy;
+        }
+    }
+}
+
+void velocity_drift_removal(Eigen::ArrayX3d &velocities) {
+    // center of mass velocity
+    Eigen::Vector3d v = velocities.colwise().sum() / velocities.rows();
+    // velocities relative to the center of mass
+    velocities.rowwise() -= v.transpose().array();
 }
 
 }  // namespace MD
--- components/initialization.h
+++ components/initialization.h
@@ -5,21 +5,51 @@
 
 namespace MD {
 
-// Initialize the positions and velocities
+// Initialize the positions in a grid
 //
 // Parameters:
-//  - positions : array of size n^3x3 where each row is one particles position
-//  - num_particles_cubic: number of particles per axis, thus the total number
-//  of particles is $n^3 = N$ which corresponds to the number of rows of the
-//  positions array
-//  - distance: distance between the particles along a coordiante axis
-//  - box_length: side length of the cubic box
+// - positions: array of size n^3x3 where each row is one particles position
+// - num_particles_cubic: number of particles per axis, thus the total number of
+//   particles is $n^3 = N$ which corresponds to the number of rows of the
+//   positions array
+// - distance: distance between the particles along a coordiante axis
+// - box_length: side length of the cubic box
+//
+// Return:
+//  Number of particles
 //
 // Description:
 //  Place the particles inside a qubic lattice of the box $[0, L]^3$.
-void init3d(Eigen::ArrayX3d &positions,
-            uint num_paricles_cubic,
-            double distance);
+uint init_potisionts_3d(Eigen::ArrayX3d &positions,
+                        uint num_paricles_cubic,
+                        double distance);
+
+// Initialize the velocities randomly
+//
+// Parameters:
+// - velocities: array of size Nx3 where N is the number of particles
+// - num_particles: number of particles, is the number of rows in the velocities
+//   array
+// - max_velocity: maximal absolute value for any velocity component
+// - seed: seed for the random number generator
+//
+// Description:
+//  Set all components of the velocities array with uniformly distributed double
+//  numbers up to the maximum velocity.
+void init_velocities_3d(Eigen::ArrayX3d &velocities,
+                        uint num_particles,
+                        double max_veloctiy,
+                        uint seed);
+
+// Remove a drift velocity
+//
+// Parameters:
+// - velocities: array of Nx3 where N is the number of particles
+//
+// Description:
+//  Compute the average net velocity and subtract it from every individual
+//  particles velocity.
+void velocity_drift_removal(Eigen::ArrayX3d &velocities);
 
 }  // namespace MD
 
