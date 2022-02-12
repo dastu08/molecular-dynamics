516a3e95a2a39fe0942a6c351d3198439a0db874
dastu08 <37212582+dastu08@users.noreply.github.com>
2022-02-12 17:13:56 +0100

    Add histogram for radial distribution function

---
+++ components/evaluation.h
@@ -0,0 +1,29 @@
+#ifndef __EVALUATION_H__
+#define __EVALUATION_H__
+
+#include <Eigen/Core>
+
+namespace MD {
+
+// Compute the histogram for the radial distribution function
+//
+// Parameters:
+// - positions: Nx3 array of the positions
+// - num_particles: number of particles, rows in positions array
+// - r_hist: vector for containing the histogram counts of the radial bins
+// - num_bins: number of bins in the radial distribution, length of r_hist
+// - box_length: side length of the box, used for minimum image convention
+//
+// Description:
+//  Loop over the pairs of particles. Compute the distance using the minimum
+//  image convention. Count the distances in bins for a radius from 0 to
+//  box_length.
+void radial_distribution_hist(Eigen::ArrayX3d& positions,
+                              uint num_particles,
+                              Eigen::VectorXi& r_hist,
+                              uint num_bins,
+                              double box_length);
+
+}  // namespace MD
+
+#endif  // __EVALUATION_H__
