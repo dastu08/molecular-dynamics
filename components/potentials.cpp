3a5bec1a80077b7e1cf1547022f7903537449662
dastu08 <37212582+dastu08@users.noreply.github.com>
2022-03-01 16:42:05 +0100

    Potential energy for MC
    Compute just the potential energy for MC.
    Update the documentation for MD.

--- components/potentials.cpp
+++ components/potentials.cpp
@@ -41,9 +41,9 @@ double lennard_jones(const Eigen::ArrayX3d &positions,
 }
 
 double lennard_jones(const Eigen::ArrayX3d &positions,
-                         Eigen::ArrayX3d &forces,
-                         uint num_particles,
-                         double side_length) {
+                     Eigen::ArrayX3d &forces,
+                     uint num_particles,
+                     double side_length) {
     double energy_total = 0;
     // auxiliary variables
     double r2_inv, r6_inv, fij;
@@ -84,3 +84,40 @@ double lennard_jones(const Eigen::ArrayX3d &positions,
 }
 
 }  // namespace MD
+
+namespace MC {
+
+double lennard_jones(const Eigen::ArrayX3d &positions,
+                     uint num_particles,
+                     double side_length) {
+    double energy_total = 0;
+    // auxiliary variables
+    double r2_inv, r6_inv;
+    Eigen::Vector3d xij;
+
+    // loop over i-j pairs of particles.
+    for (uint i = 0; i < num_particles; i++) {
+        // since i-j  and j-i are equivalent only start from j = i + 1
+        for (uint j = i + 1; j < num_particles; j++) {
+            // relative position btwn the particles
+            xij = positions.row(i) - positions.row(j);
+            // minimum image convention
+            // reduce the components to fit inside the box
+            for (uint k = 0; k < 3; ++k) {
+                xij(k) -= side_length * round(xij(k) / side_length);
+            }
+
+            // compute powers of the inverse distance
+            r2_inv = 1 / (xij.squaredNorm());
+            r6_inv = r2_inv * r2_inv * r2_inv;
+
+            // add the potential energy of the pair to the total energy
+            // only once because it already incorporates the pair of particles
+            energy_total += 4 * r6_inv * (r6_inv - 1);
+        }
+    }
+
+    return energy_total;
+}
+
+}  // namespace MC
--- components/potentials.h
+++ components/potentials.h
@@ -5,45 +5,71 @@
 
 namespace MD {
 
-// Compute the LJ energy and forces for N particles in 3 dimensions.
-//
-// Parameters:
-// - positions : array of particle positions of size Nx3, N is the number of
-//  particles
-// - forces: array of the total forces on each particle, same size as positions
-// - num_particles: number of particles, equivalent to the number of rows of the
-//  arrays positions and forces.
-//
-// Description:
-//  For each particle pair compute the forces from the Lennard-Jones potential
-//  and sum them all up. Also compute the total potential energy of the system.
-//  $V_{LJ}(r) = 4 epsilon ( (sigma/r)^{12} - (sigma/r)^6 )$
-//  The implementation has positions in units of sigma, energy in units of
-//  epsilon and forces in units of epsilon/sigma.
+/* Compute the LJ energy and forces for N particles in 3 dimensions.
+
+Parameters:
+    * positions:
+        array of particle positions of size Nx3, N is the number of particles
+    * forces:
+        array of the total forces on each particle, same size as positions
+    * num_particles:
+        number of particles, equivalent to the number of rows of the arrays
+        positions and forces.
+
+Description:
+    * For each particle pair compute the forces from the Lennard-Jones potential
+        and sum them all up.
+    * Compute the total potential energy of the system.
+        V_{LJ}(r) = 4 epsilon ( (sigma/r)^{12} - (sigma/r)^6 )
+    * positions in units of sigma.
+    * energy in units of epsilon.
+    * forces in units of epsilon/sigma.
+ */
 double lennard_jones(const Eigen::ArrayX3d &positions,
                      Eigen::ArrayX3d &forces,
                      uint num_particles);
 
-// Compute the LJ energy and forces for N particles in 3 dimensions.
-//
-// Parameters:
-// - positions : array of particle positions of size Nx3, N is the number of
-//  particles
-// - forces: array of the total forces on each particle, same size as positions
-// - num_particles: number of particles, equivalent to the number of rows of the
-//  arrays positions and forces.
-//
-// Description:
-//  For each particle pair compute the forces from the Lennard-Jones potential
-//  and sum them all up. Also compute the total potential energy of the system.
-//  $V_{LJ}(r) = 4 epsilon ( (sigma/r)^{12} - (sigma/r)^6 )$
-//  The implementation has positions in units of sigma, energy in units of
-//  epsilon and forces in units of epsilon/sigma.
+/*Compute the LJ energy and forces for N particles in 3 dimensions.
+
+Overload Parameters:
+    * side_length:
+        side length of the box for the minimum image convention
+
+Description:
+    * Use the minimum image convention for computing the relative distance.
+*/
 double lennard_jones(const Eigen::ArrayX3d &positions,
-                         Eigen::ArrayX3d &forces,
-                         uint num_particles,
-                         double side_length);
+                     Eigen::ArrayX3d &forces,
+                     uint num_particles,
+                     double side_length);
 
 }  // namespace MD
+
+namespace MC {
+
+/* Compute the LJ energy for N particles in 3 dimensions.
+
+Parameters:
+    * positions:
+        array of particle positions of size Nx3, N is the number of particles
+    * num_particles:
+        number of particles, equivalent to the number of rows of the positions
+        array
+    * side_length:
+        side length of the box for the minimum image convention
+
+Description:
+    * For each particle pair compute relative distance.
+    * Apply the minimum image convention.
+    * Compute the LJ energy
+        V_{LJ}(r) = 4 epsilon ( (sigma/r)^{12} - (sigma/r)^6 )
+    * Add everything up to the total potential energy of the system.
+    * positions in units of sigma.
+    * energy in units of epsilon.
+*/
+double lennard_jones(const Eigen::ArrayX3d &positions,
+                     uint num_particles,
+                     double side_length);
+}  // namespace MC
 
 #endif  // __POTENTIALS_H__
