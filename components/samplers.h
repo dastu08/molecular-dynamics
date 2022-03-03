e6256721e15ecb1302aec8b7ff765bf69a65cc25
dastu08 <37212582+dastu08@users.noreply.github.com>
2022-03-03 17:46:58 +0100

    MC energy and rdf sampling

--- components/samplers.h
+++ components/samplers.h
@@ -81,14 +81,50 @@ void sample_energies(uint index,
 //  side_length. Compute e_pot and e_kin using the whole system. Save in the
 //  index row of data the quantities: time, e_pot, e_kin, r_hist
 void sample_energies_rdf(uint index,
-                        double time,
-                        Eigen::ArrayX3d &positions,
-                        Eigen::ArrayX3d &velocities,
-                        double e_pot,
-                        Eigen::ArrayXXd &data,
-                        double side_length,
-                        uint num_bins);
+                         double time,
+                         Eigen::ArrayX3d &positions,
+                         Eigen::ArrayX3d &velocities,
+                         double e_pot,
+                         Eigen::ArrayXXd &data,
+                         double side_length,
+                         uint num_bins);
 
 }  // namespace MD
+
+namespace MC {
+
+/* Sample the potential energiy and the histogram of the RDF
+
+Parameters:
+    * index:
+        row index of data where the quantities will be written to
+    * positions:
+        array of particle positions of size Nx3, N is the number of particles
+    * e_pot:
+        potential energy of the whole system
+    * r_hist:
+        vector containing the histrogram counts for the RDF
+    * data:
+        array with 2 + num_bins columns where the data will be written to
+    * side_length:
+        size of the box in the minimum image convention
+    * num_bins:
+        number of bins for the radial distribution function histogram, length
+        of r_hist
+
+Description:
+    * First column is the index.
+    * Second column is e_pot
+    * Third column to last columns are elements of r_hist.
+*/
+void sample_energies_rdf(uint index,
+                         Eigen::ArrayX3d &positions,
+                         double e_pot,
+                         Eigen::VectorXi &r_hist,
+                         Eigen::ArrayXXd &data,
+                         double side_length,
+                         uint num_bins);
+
+}  // namespace MC
 
 #endif  // __SAMPLERS_H__
